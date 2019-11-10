05a69055d2f2875a9a71656e87ed9b629e0b4192
Ivan Melnikov <ivan.melnikov@deposit-solutions.com>
2019-11-10 11:13:06 +0100

    Smile tracker

---
+++ precache-manifest.5b4b5f63c3462542474697067b1b1e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55787abb7c9c88959bc3",
+    "url": "/static/css/main.bfbf6d2b.chunk.css"
+  },
+  {
+    "revision": "55787abb7c9c88959bc3",
+    "url": "/static/js/main.2e580d17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a772670fac5fb39f6a6",
+    "url": "/static/js/2.e439dfc7.chunk.js"
+  },
+  {
+    "revision": "ccd20247dfe4bef3cae08bbc4eeede84",
+    "url": "/static/media/test.ccd20247.jpg"
+  },
+  {
+    "revision": "3ffbb74a6212bde386e7ae745230149b",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.71c6ecb70f167ebe8028316cb3583430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ed0fa84100363b42aa",
+    "url": "/static/css/main.bfbf6d2b.chunk.css"
+  },
+  {
+    "revision": "60ed0fa84100363b42aa",
+    "url": "/static/js/main.37145028.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aafa9aef074e8f6ce749",
+    "url": "/static/js/2.89829e61.chunk.js"
+  },
+  {
+    "revision": "ccd20247dfe4bef3cae08bbc4eeede84",
+    "url": "/static/media/test.ccd20247.jpg"
+  },
+  {
+    "revision": "2187f530bb81ea18347f1ebeda26a12a",
+    "url": "/index.html"
+  }
+];
